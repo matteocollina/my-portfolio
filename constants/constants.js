aee504f5e159f5a7a079c20a1421f68296359a82
Matteo Collina <collinamatteo@gmail.com>
2024-01-17 22:37:56 +0100

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,62 @@
+export const NAME = 'Matteo Collina';
+
+export const DESIGNATION = `Front-end Developer | AI Lover | Music Nerd`;
+
+export const DETAILS = {
+  Residence: 'Italy',
+  City: 'Cervia',
+  Age: '30+',
+};
+
+export const SKILLS = [
+  {
+    title: 'React Developer',
+    level: '90%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '64%',
+  },
+  {
+    title: 'AI Specialist',
+    level: '53%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'collinamatteo@gmail.com',
+  PHONE: '+393331880381',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/matteocollina',
+  // LINKEDIN: 'https://www.linkedin.com/in/iosamajavaid/',
+  // TWITTER: 'https://twitter.com/iosamajavaid',
+  // FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
